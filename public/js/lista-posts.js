f323f6cc5c6bfbd1e572002ab09b03dcb1db028d
Murilo Agostinho <muriloagostinho@estudante.ufjf.br>
2026-05-13 20:12:50 -0300

    feat: adiciona animação da paginação

--- public/js/lista-posts.js
+++ public/js/lista-posts.js
@@ -23,8 +23,14 @@ function atualizarPagina(indexDaPagina) {
   const fim = inicio + cardsPorPagina;
 
   cards.forEach((card, index) => {
+    card.classList.remove("mostrar");
+
     if (index >= inicio && index < fim) {
       card.style.display = "flex";
+
+      setTimeout(() => {
+        card.classList.add("mostrar");
+      }, 10);
     } else {
       card.style.display = "none";
     }
