0933e44bf1dd97c1ec079b8319eed8582f5db635
João Pedro <joao.guadalupe06@gmail.com>
2026-05-15 10:36:41 -0300

    feat:  logica de modais

--- public/js/modais-usuarios.js
+++ public/js/modais-usuarios.js
@@ -1,9 +1,25 @@
 function abrirModal(idModal){
     document.getElementById(idModal).style.display = "flex";
+    console.log(idModal);
 }
 
 function fecharModal(idModal) {
     document.getElementById(idModal).style.display = "none";
 }
 
-const create = document.getElementById("#create");
+const createUser = document.getElementById("createUser");
+const deleteUser = document.getElementById("deleteUser");
+const viewUser = document.getElementById("viewUser");
+const editUser = document.getElementById("editUser");
+const cancel = document.getElementById("btnCancel");
+
+createUser.addEventListener("click", ()=> {
+    abrirModal("criar");
+})
+
+cancel.addEventListener("click", ()=> {
+    fecharModal("criar");
+})
+
+
+
