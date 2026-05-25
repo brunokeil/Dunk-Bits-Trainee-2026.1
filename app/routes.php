1f1ce564631d8e07d5f9e2fc75852eeab4a71a86
João Pedro <joao.guadalupe06@gmail.com>
2026-05-25 16:52:25 -0300

    feat: rota  tabela usuarios

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,11 @@
 <?php
 
 namespace App\Controllers;
+
+use App\Controllers\UsuariosController;
 use App\Controllers\ExampleController;
 use App\Core\Router;
 
+
 $router->get('', 'ExampleController@index');
+$router->get('admin-users', 'UsuariosController@index');
