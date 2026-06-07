a251d60f75d5542ff0ac29796a1ec5f918daa540
Davi <dahencosan@gmail.com>
2026-06-07 18:11:20 -0300

    fix: corrigindo path de estilização do login

--- app/views/site/login.view.php
+++ app/views/site/login.view.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>LOGIN</title>
 
-		<link rel="stylesheet" href="../../../public/css/login.css" />
+		<link rel="stylesheet" href="../../../public/css/login.css"/>
 	</head>
 
 	<body class="telaLogin">
@@ -16,8 +16,8 @@
 				<ion-icon name="close-outline"></ion-icon>
 			</a>
 
-			<h2>LOGIN</h2>
-			
+			<h2 id="tituloLogin">LOGIN</h2>
+
 			<form action="/login" method="POST">
 				<div class="inputBox">
                     <label>
