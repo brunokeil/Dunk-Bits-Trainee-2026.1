a948ef94c2a4fb37f5508ee2f36d50eac34858a5
Davi <dahencosan@gmail.com>
2026-06-07 22:44:44 -0300

    feat: adicionando routes de cadastro, dashboard, cadastro/create.

--- app/routes.php
+++ app/routes.php
@@ -2,8 +2,13 @@
 
 namespace App\Controllers;
 use App\Controllers\ExampleController;
+use App\Controllers\CadastroController;
 use App\Core\Router;
 
 $router->get('', 'ExampleController@index');
 
-$router->get('cadastro', 'CadastroController@index');
+$router->get('cadastro', 'CadastroControllerGET@index');
+
+$router->get('dashboard', 'DashboardController@index');
+
+$router->post('cadastro/create', 'CadastroControllerPOST@criar');
