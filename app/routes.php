757860af472f70be6c5df5ca550605ae8a83dd30
Davi <dahencosan@gmail.com>
2026-06-09 23:30:49 -0300

    feat: adicionando rotas para navbar e landingpage.

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,12 @@
 
 namespace App\Controllers;
 use App\Controllers\ExampleController;
+use App\Controllers\NavbarController;
+use App\Controllers\LandingPageController;
 use App\Core\Router;
 
 $router->get('', 'ExampleController@index');
+
+$router->get('navbar', 'NavbarController@index');
+
+$router->get('landingpage', 'LandingPageController@index');
