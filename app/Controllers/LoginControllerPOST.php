d2a444561a5fc8530549b32881697131136b9a70
Davi <dahencosan@gmail.com>
2026-06-11 14:37:33 -0300

    fix: arrumando PHP.

--- app/Controllers/LoginControllerPOST.php
+++ app/Controllers/LoginControllerPOST.php
@@ -12,16 +12,14 @@ class LoginControllerPOST
     {
         $email = $_POST['email'];
         $senha = $_POST['senha'];
-
         $user = App::get(key: 'database')->verificaLogin($email, $senha);
-
         if($user){
             session_start();
             $_SESSION['id'] = $user->id;
             header('Location: /dashboard');
             exit();
         }else{
-             $_SESSION['mensagem-erro'] = "Usuário e/ou senha incorretos";  
+             $_SESSION['mensagem-erro'] = "Usuário e/ou senha incorretos";
              header('Location: /login');
              exit();
          }
