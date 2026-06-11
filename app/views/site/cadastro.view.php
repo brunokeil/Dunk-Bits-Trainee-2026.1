d7f39ddb18ede0393fb12e81cde0e85048ce11a7
Davi <dahencosan@gmail.com>
2026-06-11 14:36:28 -0300

    fix: arrumando redirecionamento e MVC.

--- app/views/site/cadastro.view.php
+++ app/views/site/cadastro.view.php
@@ -1,5 +1,4 @@
   <!-- FLUMINENSE -->
-
 <!doctype html>
 <html lang="pt-BR">
 	<head>
@@ -9,37 +8,34 @@
 
 		<link rel="stylesheet" href="../../../public/css/cadastro.css" />
 	</head>
-
 	<body class="telaLogin">
 		<main class="loginContainer">
-			<a href="#" class="buttonFechar">
+			<a href="/landingpage" class="buttonFechar">
 				<ion-icon name="close-outline"></ion-icon>
 			</a>
-			<h2 id="tituloCadastro">CADASTRO</h2>  
+			<h2 id="tituloCadastro">CADASTRO</h2>
 			<form action="/cadastro/create" method="POST">  
 				<div class="inputBox">
 					<input type="text" name="name" placeholder="NOME" />
 				</div>
-
 				<div class="inputBox">
 					<input type="text" name="email" placeholder="EMAIL" />
 				</div>
 				<div class="inputBox">
                     <label>
-					    <input name="senha" id="senha" type="password" placeholder="SENHA" onkeyup='checar()'>
-						<ion-icon name="eye-off-outline" class="olho" id="olhoSenha" onclick="mostrarSenha()"></ion-icon>
+					    <input name="senha" id="senha" type="password" placeholder="SENHA">
+						<ion-icon name="eye-off-outline" class="olho" id="olhoSenha"></ion-icon>
                     </label>
 				</div>
 				<div class="inputBox">
 				    <label>
 					    <input name="confirmarsenha" id="confirmarsenha" type="password" placeholder="CONFIRMAR SENHA" onkeyup='checar()'>
-						<ion-icon name="eye-off-outline" class="olho" id="olhoConfirmarSenha" onclick="mostrarConfirmarSenha()"></ion-icon>
+						<ion-icon name="eye-off-outline" class="olho" id="olhoConfirmarSenha"></ion-icon>
                     </label>
 				</div>
 				<button type="submit" class="buttonEntrar">CRIAR</button>
 			</form>
 		</main>
-
 		<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 		<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js" ></script>
 		<script src="../../../public/js/cadastro.js"></script>
