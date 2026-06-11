6d1e9ec2848c80bc764459dfbbf9c4cb942b2006
DaviSinon <davi.santos@estudante.ufjf.br>
2026-06-10 22:28:16 -0300

    fix: concertando funções que não deviam estar no login.

--- public/js/login.js
+++ public/js/login.js
@@ -4,46 +4,11 @@ function mostrarSenha(){
 
     if(inputSenha.type === 'password'){
         inputSenha.setAttribute('type', 'text');
-        btnMostrarSenha.classList.replace('eye-outline','eye-off-outline')
-    } else{
-        inputSenha.setAttribute('type', 'password');
-        btnMostrarSenha.classList.replace('eye-off-outline','eye-outline')
-    }
-}
-function mostrarConfirmarSenha(){
-    var inputConfirmarSenha = document.getElementById('confirmarsenha')
-    var btnMostrarConfirmarSenha = document.getElementById('olhoConfirmarSenha')
-
-    if(inputConfirmarSenha.type === 'password'){
-        inputConfirmarSenha.setAttribute('type', 'text');
-        btnMostrarConfirmarSenha.setAttribute('name', 'eye-outline');
+        btnMostrarSenha.setAttribute('name', 'eye-outline');
     }else{
-        inputConfirmarSenha.setAttribute('type', 'password');
-        btnMostrarConfirmarSenha.setAttribute('name', 'eye-off-outline');
+        inputSenha.setAttribute('type', 'password');
+        btnMostrarSenha.setAttribute('name', 'eye-off-outline');
     }
 }
 
-document.getElementById('confirmarsenha').addEventListener('input', function(){ checar(); });
 
-function checar(){
-    const senha = document.getElementById('senha').value;
-    const confirmarsenha = document.getElementById('confirmarsenha').value;
-    const logar = document.querySelector('.buttonEntrar');
-    const caixaborda1 = document.getElementById('confirmarsenha');
-    const caixaborda2 = document.getElementById('senha');
-
-
-    if(!senha || !confirmarsenha){
-        caixaborda1.style.border = "2px solid white";
-        caixaborda2.style.border = "2px solid white";
-        logar.disabled = true;
-    }else if(senha !== confirmarsenha){
-        caixaborda1.style.border = "3px solid red";
-        caixaborda2.style.border = "3px solid red";
-        logar.disabled = true;
-    } else{
-        caixaborda1.style.border = "2px solid green";
-        caixaborda2.style.border = "2px solid green";
-        logar.disabled = false;
-    }
-}
