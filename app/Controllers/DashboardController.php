5b0c181f38fb33c9ef43f6239e05dc12db381989
Davi <dahencosan@gmail.com>
2026-06-12 01:37:01 -0300

    feat: adicionando conferência de login na dashboard controller.

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -7,9 +7,11 @@ use Exception;
 
 class DashboardController
 {
-
     public function index()
     {
+        if(!isset($_SESSION['id'])){
+            header(header: 'Location: /login');
+        }
         return view('admin/dashboard');
     }
 }
