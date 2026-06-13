67e990fce4d87b7fd0aa3763ce1b18e7d5fc84de
DaviSinon <davi.santos@estudante.ufjf.br>
2026-06-13 14:49:49 -0300

    fix: concertando footer.

--- app/views/site/footer.view.php
+++ app/views/site/footer.view.php
@@ -1,14 +1,4 @@
-<!-- FLUMINENSE-->
 
-<!doctype html>
-<html lang="pt-BR">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>FOOTER</title>
-        <link rel="stylesheet" href="../../../public/css/footer.css" />
-    </head>
-    <body class="telaFooter">
         <footer class="rodape">
             <div class="acima">
                 <div class="social">
@@ -49,13 +39,6 @@
                 <p class="buttonValores">VALORES</p>
             </div>
         </footer>
-    </body>
-    <script
-        type="module"
-        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
-    ></script>
-    <script
-        nomodule
-        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
-    ></script>
-</html>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"> </script> 
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
