59a77bd845642375c4dd35c21dafeda43e15c173
DaviSinon <davi.santos@estudante.ufjf.br>
2026-06-13 02:50:19 -0300

    refactor: transoformando controller de login em uma única controller.

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class LoginController
+{
+
+    public function LoginView()
+    {
+        if (isset($_SESSION['id'])) {
+            header(header: 'Location: /dashboard');
+        }
+
+        $redirectLabel = '/dashboard';
+
+
+        if (isset($_REQUEST['redirect'])) {
+            $redirectLabel = $_REQUEST['redirect'];
+        }
+        return view('site/login', ["redirect" => $redirectLabel]);
+    }
+
+    public function logar()
+    {
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $redirect = $_POST['redirect'] ?? "/dashboard";
+
+        $user = App::get(key: 'database')->verificaLogin($email, $senha);
+
+        if ($user) {
+            $_SESSION['id'] = $user->id;
+            header("Location: " . $redirect);
+            exit();
+        } else {
+            header('Location: /login');
+            exit();
+        }
+    }
+}
