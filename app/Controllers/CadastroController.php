8cf20131c4eb521675346a4811591a61fc1e204f
DaviSinon <davi.santos@estudante.ufjf.br>
2026-06-13 12:52:16 -0300

    refactor: mudando lógica cadastro.

--- app/Controllers/CadastroController.php
+++ app/Controllers/CadastroController.php
@@ -27,10 +27,12 @@ class CadastroController
 
         if($parameters['name'] && $parameters['email'] && $parameters['password'] && ($confirmarsenha === $parameters['password'])){
             App::get('database')->insert('users', $parameters);
+            session_start();
             $user = App::get(key: 'database')->verificaLogin($parameters['email'], $parameters['password']);
             $_SESSION['id'] = $user->id;
             header('Location: /dashboard');
         } else{
+            session_start();
             header('Location: /cadastro');
         }
     }
