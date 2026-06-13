0c93fc2a04b003e89e514f74a96e16452d679776
DaviSinon <davi.santos@estudante.ufjf.br>
2026-06-13 12:53:13 -0300

    refactor: mudando lógica login.

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -10,34 +10,36 @@ class LoginController
 
     public function LoginView()
     {
-        if (isset($_SESSION['id'])) {
-            header(header: 'Location: /dashboard');
+        session_start();
+        if(isset($_SESSION['id'])) {
+            header('Location: /dashboard');
         }
 
-        $redirectLabel = '/dashboard';
+        // $redirectLabel = 'dashboard';
 
 
-        if (isset($_REQUEST['redirect'])) {
-            $redirectLabel = $_REQUEST['redirect'];
-        }
-        return view('site/login', ["redirect" => $redirectLabel]);
+        // if(isset($_REQUEST['redirect'])) {
+        //     $redirectLabel = $_REQUEST['redirect'];
+        // }
+        return view('site/login');
     }
+
 
     public function logar()
     {
         $email = $_POST['email'];
         $senha = $_POST['senha'];
-        $redirect = $_POST['redirect'] ?? "/dashboard";
+        // $redirect = $_POST['redirect'] ?? "dashboard";
 
         $user = App::get(key: 'database')->verificaLogin($email, $senha);
 
         if ($user) {
+            session_start();
             $_SESSION['id'] = $user->id;
-            header("Location: " . $redirect);
-            exit();
+            header("Location: /dashboard");
         } else {
+            session_start();
             header('Location: /login');
-            exit();
         }
     }
 }
