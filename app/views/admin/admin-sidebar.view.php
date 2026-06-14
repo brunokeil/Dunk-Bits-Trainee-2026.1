0d8745b474fa868fc30a936cf5f8b30b0706b69c
matt-macs <matt.almeidac@gmail.com>
2026-06-14 18:00:13 -0300

    feat: add sidebar

---
+++ app/views/admin/admin-sidebar.view.php
@@ -0,0 +1,66 @@
+<link rel="stylesheet" href="/public/css/sidebar.css" />
+
+  <aside class="sidebar" id="sidebar">
+    <div class="top-sidebar">
+      <div class="top-top-sidebar">
+        <div class="setinha">
+          <button id="toggle-sidebar-btn">
+            <img id="setinha" src="/public/assets/ArrowLeft.svg" />
+          </button>
+        </div>
+        <div class="user-space-sidebar">
+          <img id="user-icon" src="/public/assets/PersonCircle.svg" />
+          <div>
+            <p id="user-name">Lorem Ipsum</p>
+            <p id="cargo">Adm</p>
+          </div>
+        </div>
+      </div>
+      <div class="admin-sidebar">
+        <p>Admin</p>
+        <nav class="content-sidebar">
+          <ul>
+            <li>
+              <a href="#">
+                <span class="item-sidebar">
+                  <img id="chevette" src="/public/assets/chevette-left.svg" />
+                  Voltar à Página Inicial
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="item-sidebar">
+                  <img id="chevette" src="/public/assets/chevette-left.svg" />
+                  Acessar Dashboard
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="item-sidebar">
+                  <img id="chevette" src="/public/assets/chevette-left.svg" />
+                  Gerenciar Posts
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="item-sidebar">
+                  <img id="chevette" src="/public/assets/chevette-left.svg" />
+                  Gerenciar Usuários
+                </span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div class="logout-sidebar">
+      <button type="button" id="logout-btn">
+        Sair
+      </button>
+    </div>
+  </aside>
+  
+<script src="/public/js/sidebar.js"></script>
