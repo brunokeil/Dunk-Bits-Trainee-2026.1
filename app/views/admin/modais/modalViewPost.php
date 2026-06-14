6826d669c6d8ccb661d8bf1e9114e9e56e97254f
DaviSinon <davi.santos@estudante.ufjf.br>
2026-06-14 03:58:20 -0300

    fix: concertando e deixando usável o modal de view de posts.

--- app/views/admin/modais/modalViewPost.php
+++ app/views/admin/modais/modalViewPost.php
@@ -1,4 +1,4 @@
-<div class="modalPost close" id="modalView">
+<div class="modalPost close" id="modalViewPost-<?= $post->id?>">
 
   <span id="header">
     <h1>PERFIL DE POST</h1>
@@ -8,55 +8,23 @@
   <div id="content">
 
     <div class="postInfo">
-
       <div class="postPicture">
-        <img
-          src="/public/assets/placeholder/blank-prof-pic.png "
-          class="imgPostPic imgProfPic" />
-        <input type="file" class="profileInput" accept="image/*" hidden />
+        <img src="/public/assets/placeholder/blank-prof-pic.png" class="imgPostPic imgProfPic"/>
+        <input type="file" class="profileInput" accept="image/*" hidden/>
       </div>
-
       <div class="authorData">
-
         <div class="inputGroup">
           <ion-icon name="person"></ion-icon>
-
-          <input
-            type="text"
-            class="postAuthor"
-            placeholder="Username"
-            value="Paula Tejano"
-            disabled
-            id="viewAuthor" />
+          <input type="text" class="postAuthor" placeholder="Username" value="<?= $post->author_name?>" disabled id="viewAuthor"/>
         </div>
-
         <div class="inputGroup">
-
-          <input
-            type="text"
-            class="postDate"
-            placeholder="Data"
-            value="11/06/2014"
-            disabled
-            id="viewDate" />
+          <input type="text" class="postDate" placeholder="Data" value="<?= $post->created_at?>" disabled id="viewDate" />
         </div>
       </div>
-
     </div>
-
     <div class="postContent">
-
-      <textarea
-        class="titleInput"
-        placeholder="Titulo do post"
-        id="viewTitle"></textarea>
-
-      <textarea
-        class="descriptionInput"
-        placeholder="Descrição"
-        id="viewDescription"></textarea>
-
-
+      <textarea class="titleInput" id="viewTitle"><?=$post->title?></textarea>
+      <textarea class="descriptionInput" id="viewDescription"><?= $post->content?></textarea>
     </div>
   </div>
 
