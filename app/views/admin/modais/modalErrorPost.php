c4a2b2a9e4c82b96f79531789467bbd42c316713
matt-macs <matt.almeidac@gmail.com>
2026-06-16 09:00:59 -0300

    feat: add modal de erro + ajustes de estilo

---
+++ app/views/admin/modais/modalErrorPost.php
@@ -0,0 +1,22 @@
+<form class="modalPost warningModal close" id="modalDeletePost-<?= $post->id ?>" action="/postsadmin/delete" method="POST">
+  <div class="warningHeader">
+    <ion-icon name="warning"></ion-icon>
+
+    <span>
+      <h1>Erro</h1>
+    </span>
+  </div>
+
+  <p>
+    Ocorreu um erro ao tentar criar o post.
+    <br />
+    <br />
+
+    Lembre-se que todos os campos são obrigatórios.
+  </p>
+  <input type="hidden" name="id" value="<?= $post->id ?>" />
+  <div class="footerButtons">
+    <button type="button" class="closeModalBtn cancelBtn">CANCELAR</button>
+    <button type="submit" class="primaryBtn">EXCLUIR</button>
+  </div>
+</form>
