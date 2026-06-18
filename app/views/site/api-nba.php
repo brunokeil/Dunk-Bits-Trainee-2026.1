fdfea173dc6b5044d7e0aa674d7316eda95ce434
matt-macs <matt.almeidac@gmail.com>
2026-06-18 15:49:54 -0300

    feat: :sparkles: implementação do front da api

---
+++ app/views/site/api-nba.php
@@ -0,0 +1,30 @@
+<section class="api">
+    <div class="icones">
+        <ion-icon class="casinha" name="home"></ion-icon>
+        <ion-icon name="people"></ion-icon>
+    </div>
+
+    <div class="jogo">
+        <h4 class="data">11-08-2026</h5>
+        <div>
+
+            <h4 class="time casa">time foda aqui grande
+                <br />
+                (sigla)
+            </h4>
+            <img class="logo-time" src="public/assets/placeholder/blank-profile-pic.webp" />
+            
+            <div class="pontos">
+                <h2>00</h2>
+                <p>X</p>
+                <h2>00</h2>
+            </div>
+            
+            <img class="logo-time" src="public/assets/placeholder/blank-profile-pic.webp" />
+            <h4 class="time visitante">time foda aqui grande
+                <br />
+                (sigla)
+            </h4>
+        </div>
+    </div>
+</section>
