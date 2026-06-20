79d327df0b567c1e57d9ce81039269815335e4bf
Davi <dahencosan@gmail.com>
2026-06-20 05:14:31 -0300

    feat: adicionando função de hash.

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -12,6 +12,7 @@ class LoginController
     {
         if (isset($_SESSION['id'])) {
             header(header: 'Location: /dashboard');
+            exit();
         }
 
         $redirectLabel = '/dashboard';
@@ -28,18 +29,28 @@ class LoginController
     public function logar()
     {
         $email = $_POST['email'];
-        $senha = $_POST['senha'];
+        $senhaInput = $_POST['senha'];
         $redirect = $_POST['redirect'] ?? "dashboard";
 
-        $user = App::get(key: 'database')->verificaLogin($email, $senha);
+        $user = App::get(key: 'database')->selectOneEmail('users', $email);
 
-        if ($user){
-            $_SESSION['id'] = $user->id;
-            $_SESSION['is_admin'] = $user->is_admin;
-            header("Location: " . $redirect);
-        } else {
+        if(!$user){
             $_SESSION['mensagem-erro'] = "Usuário e/ou senha incorretos";
             header('Location: /login');
+            exit();
+        }
+
+        $senhaVerificada = password_verify($senhaInput, $user->password);
+
+        if(!$senhaVerificada){
+            $_SESSION['mensagem-erro'] = "Usuário e/ou senha incorretos";
+            header('Location: /login');
+            exit();
+        }else{
+            $_SESSION['id'] = $user->id;
+            $_SESSION['is_admin'] = $user->is_admin;
+            header("Location: " . $redirect);    
+            exit();
         }
     }
         public function deslogar()
@@ -47,5 +58,6 @@ class LoginController
         session_unset();
         session_destroy();
         header('Location: /login');
+        exit();
     }
 }
