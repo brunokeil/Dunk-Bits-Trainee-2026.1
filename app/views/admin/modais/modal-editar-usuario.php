fc65652e7ac073c9a9b8ba72bd9f1209ac14d4c2
João Pedro <joao.guadalupe06@gmail.com>
2026-06-21 20:44:11 -0300

    fix: mensagem imagem

--- app/views/admin/modais/modal-editar-usuario.php
+++ app/views/admin/modais/modal-editar-usuario.php
@@ -14,8 +14,7 @@
             <input type="file" name="imagem" class="profileInput" accept="image/*" hidden />
             <ion-icon class="icone-img" name="image-outline"></ion-icon>
             <h4 class="txt-img">
-                Arraste sua imagem aqui
-                ou clique para escolher um arquivo
+                Clique para escolher um arquivo
             </h4>
 
         </div>
